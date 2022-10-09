dd2605656ec73c0346b469a0385eb28e124432e4
nihal <nihal.vish29@gmail.com>
2022-10-09 18:18:41 +0530

    dashboard bottom navigation implementation

--- app/src/main/java/com/example/dev_until_50/user_details.java
+++ app/src/main/java/com/example/dev_until_50/user_details.java
@@ -1,10 +1,17 @@
 package com.example.dev_until_50;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class user_details extends AppCompatActivity {
+
+    AppCompatButton signup_btn,googlesignup_btn;
+    EditText email_edt,firstname_edt,lastname_edt,phonenumber_edt,password_edt,cpassword_edt;
+    TextView login_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
